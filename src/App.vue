5b333d211e9e017bf7f2dfb7b27100c56aa7e8fd
swimming <48545779+swimming123@users.noreply.github.com>
2024-05-29 19:03:22 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <page-header/>
+    <router-view/>
+    <page-footer/>
+  </div>  
+</template>
+<script>
+import PageHeader from "./components/PageHeader.vue";
+import PageFooter from "./components/PageFooter.vue";
+export default {
+  name:"App",
+  components: { PageHeader,PageFooter},  
+};
+</script>
+<style>
+</style>
